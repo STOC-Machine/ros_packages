e6ee00499f46a12d37c8fef0018d2f3dd9c4d405
Thong Vo <vo4@stolaf.edu>
2019-07-29 04:01:43 -0500

    Add more commands and enable uppercase cases

--- offb_py/src/analyze_command.py
+++ offb_py/src/analyze_command.py
@@ -2,24 +2,24 @@
 import rospy
 import csv 
 
-# csv file name 
 
-# 0: outcome, 5: batter, 7: inning, 10: top, which team is playing
 import pathlib
 
 DroneName = {'red': 0, 'blue': 1, 'purple': 2, 'orange': 3}
-Action = {"fly": 0, "turn": 1, "stop": 2, "save": 3, "special": 4}
-MoveDirection = {"forward": 0, "backward": 1, "up": 2, "down": 3, "right": 4, "left": 5, "to": 6}
+Action = {"fly": 0, "turn": 1, "stop": 2, "save": 3, "screenshot": 4, "special": 7}
+MoveDirection = {"forward": 0, "backward": 1, "back": 1, "up": 2, "down": 3, 
+"right": 4, "left": 5, "to": 6, "north":7, "south": 8, "east": 9, "west": 10}
 # 3DTurn = {"x y": 0, "x z": 1, "y z": 2}
-TurnDirection = {"counter": 0, "clockwise": 1}
+# Mode = {"attack": 0, "defense": 1}
+TurnDirection = {"counter-clockwise": 0, "counter": 0, "clockwise": 1, "back": 2}
 
 Port = 12459
 
 def analyze_command(command):
     # Find the drone info
     words = command.split()
-    
-    # TODO: Catch error
+    words = [word.lower() for word in words]
+
     try:
         index = 0
         
@@ -43,15 +43,11 @@ def analyze_command(command):
             direction = 0
         elif words[index] == "save":
             direction = 0
+        elif words[index] == "screenshot":
+            direction = 0
         elif words[index] == "special":
             direction = (1,1,1)
-        # Obtain commands
 
-        # Obtain direction
-        # start = words.find("fly")
-        # direction = Direction[ words[start+1]]
-
-        print((no_drone, action, direction))
         return (no_drone, action, direction)
         # TODO: Consider velocity
     except KeyError:
@@ -62,7 +58,7 @@ def analyze_command(command):
         return (-1,0,0)
 
 import socket
-from cmd_pub import cmd_pub
+# from cmd_pub import cmd_pub
 
 def start_listen():
     # create cmd_pub object
@@ -96,34 +92,26 @@ def start_listen():
                     command_publisher.publish(command_tmp)
                     break
             #print "a"
-            #command_publisher.publish(analyze_command(message))
+            command_publisher.publish(analyze_command(message))
             #print "b"
             data = "" 
         #print "c"
         conn.close()
 
-# start_listen()
+# Testing
+# analyze_command("blue fly forward")
+# analyze_command("red save me")
+# analyze_command("orange stop")
+# analyze_command("bluefly up")
+# analyze_command("redfly to me")
+# analyze_command("blue fly")
 
-# def send_message():
-#     ## Connect to an IP with Port, could be a URL
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     sock.connect(('192.168.0.165', 12459))
-#     ## Send some data, this method can be called multiple times
-#     sock.send(bytes("Twenty-five bytes to send", 'utf-8'))
-#     ## Receive up to 4096 bytes from a peer
-#     sock.recv(4096)
-#     ## Close the socket connection, no more data transmission
-#     # sock.close()
-
-# send_message()
-#analyze_command("blue fly forward")
-#analyze_command("red save me")
-#analyze_command("orange stop")
-#analyze_command("bluefly up")
-#analyze_command("redfly to me")
-#analyze_command("blue fly")
-
-
+# analyze_command("blue FLY forward")
+# analyze_command("red SAVE me")
+# analyze_command("ORANGE stop")
+# analyze_command("blue screenshot")
+# analyze_command("red fly TO ME")
+# analyze_command("blue fly")
 
 ########        main program        ########
 if __name__ == "__main__":
@@ -132,10 +120,10 @@ if __name__ == "__main__":
     # run loop to listen for commands
     start_listen()
 
-    # test cmd_pub
-    #command_publisher = cmd_pub()
-    #command_publisher.publish(analyze_command("orange stop"))
-    #command_publisher.publish((0,1,1))
+    test cmd_pub
+    command_publisher = cmd_pub()
+    command_publisher.publish(analyze_command("orange stop"))
+    command_publisher.publish((0,1,1))
 
 
 
